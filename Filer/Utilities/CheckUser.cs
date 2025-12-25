2ede0af19a07873c7db5888df25f43a043127743
Mahdi Mcheik <mahdi.mcheik@hotmail.fr>
2025-12-25 02:40:29 +0100

    check claims

---
+++ Filer/Utilities/CheckUser.cs
@@ -0,0 +1,11 @@
+﻿using System.Security.Claims;
+
+namespace Filer.Utilities;
+
+public static class CheckUser
+{
+    public static List<Claim> GetInfosFromClaims(ClaimsPrincipal principal)
+    {
+        return principal.Claims.ToList();
+    }
+}
